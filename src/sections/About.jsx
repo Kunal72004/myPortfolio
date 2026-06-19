33135f83712e8eea981daa7618820b2392c1b2dd
Kunal72004 <143131261+Kunal72004@users.noreply.github.com>
2026-06-20 00:16:24 +0530

    working on about page

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import { FaRegEnvelopeOpen } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 const About = () => {
-  return (
-    <div>
-      About
-    </div>
-  )
-}
+  const services = [
+    {
+      title: "Frontend Development",
+      description:
+        "Creating responsive and interactive user interfaces using React and Tailwind CSS.",
+    },
+    {
+      title: "Backend Development",
+      description: "Building scalable REST APIs using Node.js and Express.js.",
+    },
+    {
+      title: "Database Management",
+      description:
+        "Designing efficient MongoDB schemas and handling data operations.",
+    },
+    {
+      title: "DSA & Problem Solving",
+      description:
+        "Strong understanding of algorithms, data structures and coding challenges.",
+    },
+  ];
 
-export default About
+  return (
+    <section id="about" className="py-20">
+      <div className="max-w-7xl mx-auto ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          {/* left */}
+          <div className="border border-gray-500">
+            <div>
+              <h2 className="text-4xl font-bold">
+              About <span className="text-violet-500 ">Me</span>
+            </h2>
+            <div className="border-2 rounded-lg mt-2 border-violet-500 w-10"></div>
+            </div>
+            
+            <p className="text-gray-400 mt-6 max-w-lg">
+              {" "}
+              I'm a passionate MERN Stack Developer with a strong foundation in
+              Data Structures and Algorithms. I enjoy building scalable web
+              applications, solving real-world problems and continuously
+              learning new technologies.
+            </p>
+            <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-3 lg:gap-4 ">
+              <div className="flex border rounded-md gap-4 items-center justify-center border-gray-600/40 py-2 bg-gray-700/20 hover:bg-gray-950">
+                <FaLocationDot />
+                <div className="flex-col ">
+                  <p className="text-gray-400">Location</p>
+                  <p className="font-semibold">India</p>
+                </div>
+              </div>
+              <div className="flex border rounded-md gap-4 items-center justify-center border-gray-600/40 py-2 bg-gray-700/20 hover:bg-gray-950">
+                <FaRegEnvelopeOpen />
+                <div className="flex-col ">
+                  <p className="text-gray-400">Experience</p>
+                  <p className="font-semibold">Fresher</p>
+                </div>
+              </div>
+              <div className="flex border rounded-md gap-4 items-center justify-center border-gray-600/40 py-2 bg-gray-700/20 hover:bg-gray-950">
+                🟢
+                <div className="flex-col ">
+                  <p className="text-gray-400">Availability</p>
+                  <p className="font-semibold text-sm">Open To Work</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* right */}
+          <div className="border border-gray-600/40 bg-gray-700/20 p-3 rounded-lg shadow-lg shadow-violet-500/20"> 
+            <h3 className="font-semibold text-xl">
+              What I Do
+            </h3>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -5,34 +5,34 @@ import Profile from '../assets/portfolio_profile_img.png'
 
 const Home = () => {
   return (
-    <section id="home" className="pt-20 max-h-screen">
-      <div className="max-w-7xl mx-auto h-[calc(100vh-80px)] border border-amber-300">
-        <div className="grid grid-cols-1 lg:grid-cols-2 h-full ">
+    <section id="home" className="pt-20 min-h-screen">
+      <div className="max-w-7xl mx-auto min-h-[calc(100vh-80px)] border border-amber-300">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
           {/* left section */}
-          <div className="border border-blue-700 flex flex-col justify-center">
+          <div className="border border-blue-700 flex flex-col justify-center items-start order-1">
             <span className="text-violet-400 text-xl font-medium ">
               <span className="text-2xl">👋</span> Hello, I'm
             </span>
-            <h1 className="font-space text-6xl mt-4">
+            <h1 className="font-space text-5xl lg:text-7xl mt-4">
               Kunal
               <br /> Sikarwar
             </h1>
-            <h2 className="text-2xl font-medium mt-4">
+            <h2 className="text-2xl font-semibold mt-4">
               <span className="text-violet-500">MERN</span> Stack Developer
             </h2>
-            <p className="text-gray-400 mt-6 max-w-md">
+            <p className="text-gray-400 mt-6 max-w-lg">
               I build modern, scalable and responsive web applications using
               MongoDB, Express.js, React.js and Node.js.
             </p>
             <div className="flex gap-4 mt-8 ">
-              <button className="bg-linear-to-br from-indigo-500 to-violet-500 px-6 py-3 flex gap-1 items-center rounded-md font-medium hover:scale-105 transition-all duration-300">
+              <button className="bg-linear-to-br from-indigo-500 to-violet-500 px-6 py-3 flex gap-1 items-center rounded-md font-medium hover:scale-105 shadow-lg hover:shadow-violet-500/20 transition-all duration-300 ">
                 View Projects <FaLongArrowAltRight />
               </button>
-              <button className="border border-violet-500/60 px-6 py-3 flex gap-1 items-center rounded-md hover:border-violet-500/30 hover:scale-105 transition-all duration-300">
+              <button className="border border-violet-500/60 px-6 py-3 flex gap-1 items-center rounded-md hover:border-violet-500/30 hover:scale-105 hover:bg-violet-500/10 transition-all duration-300">
                 Download Resume <FaLongArrowAltRight />
               </button>
             </div>
-            <div className="flex gap-6 mt-5 text-xl px-2">
+            <div className="flex gap-6 mt-5  md:text-xl px-2">
               <a href="https://github.com/Kunal72004">
                 <FaGithub />
               </a>
@@ -47,11 +47,12 @@ const Home = () => {
               </a>
             </div>
           </div>
+
           {/* right section */}
-          <div className="border border-red-700 flex justify-center items-center">
-            <div className="overflow-hidden border border-rose-700">
+          <div className="border border-red-700 flex justify-center items-center order-2">
+            <div className="overflow-hidden border w-75 md:w-95 lg:w-full border-rose-700">
               <img className="w-full h-full object-contain" src={Profile} alt="profile img" />
-            </div>
+            </div> 
           </div>
         </div>
       </div>
@@ -60,24 +61,3 @@ const Home = () => {
 };
 
 export default Home;
-
-{
-  /* <div className="p-4 flex flex-col gap-2 mt-10 font-light max-w-fit">
-        <div className="text-xl text-violet-500">Hi, I'm</div>
-        <div className="text-4xl font-bold font-space">Kunal Sikarwar</div>
-        <div className="text-xl"> <span className="text-violet-500 ">MERN</span> Stack Developer</div>
-        <div className="text-xs">
-          Third-year B.Tech Computer Science Engineering student with strong
-          interest in software development and core Computer Science. Skilled in
-          Java, DSA, and Web Development (MERN) , seeking real-world
-          opportunities.
-        </div>
-        <div className="flex gap-4">
-          <button>View My Projects</button>
-          <button>Contact Me</button>
-        </div>
-
-        <div>social icons</div>
-      </div>
-      <div className="bg-yellow-700 ">kunal</div> */
-}
